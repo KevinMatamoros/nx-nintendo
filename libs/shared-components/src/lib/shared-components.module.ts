f189158dc894417b73f1ce9849b9a6cd7c596d9e
Kevin Matamoros Serrano <kmatamoros@consultec-ti.com>
2023-09-03 17:18:40 -0600

    feat: shared header

--- libs/shared-components/src/lib/shared-components.module.ts
+++ libs/shared-components/src/lib/shared-components.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './presentational/header/header.component';
+import { HeaderComponent } from './presentational';
 
 @NgModule({
   imports: [CommonModule],
